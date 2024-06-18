6683899d5a4976cd22cac6882ec4bfc1ba6bc854
Rickson Costa <149190460+Rickson-Costa@users.noreply.github.com>
2024-06-18 17:25:14 -0300

    Update Configurando arquivo LDAP.py

--- Códigos Soltos/Configurando arquivo LDAP.py
+++ Códigos Soltos/Configurando arquivo LDAP.py
@@ -9,6 +9,36 @@ logger = logging.getLogger(__name__)
 LDAP_IP_SERVER = 'COLOQUE O IP DO LDAP' #Este é o endereço IP do servidor LDAP.
 LDAP_DOMAIN_NAME = 'COLOQUE O DOMINIO' #Este é o nome de domínio do servidor LDAP
 
+# Script para procurar em uma coluna e subtituir na coluna do lado
+
+# Carregar a planilha de consulta
+consulta_df = pd.read_excel('Resultado.xlsx', sheet_name='Planilha1')
+
+# Carregar a planilha de resultado (vazia inicialmente)
+resultado_df = pd.read_excel('dados filtrados.xlsx', sheet_name='42365')
+N = 0
+
+# Verificar o cadastro do usuário na planilha de consulta
+for index, row in consulta_df.iterrows():
+    usuario = row.iloc[0]  # Primeira coluna da planilha de consulta (usuário)
+    dado = row.iloc[7]     # Oitava coluna da planilha de consulta (dado a ser copiado)
+    
+    # Verifica se o usuário está na planilha de resultado
+    if usuario in resultado_df.iloc[:, 0].values:
+        # Encontra o índice onde o usuário está na planilha de resultado
+        idx = resultado_df.index[resultado_df.iloc[:, 0] == usuario][0]
+        
+        # Copia o dado da 5ª coluna da planilha de consulta para a 5ª coluna da planilha de resultado
+        resultado_df.iloc[idx, 7] = dado
+        print(f"Encontrei e alterei {N}")
+        N += 1
+
+# Salvar a planilha de resultado atualizada
+resultado_df.to_excel('dados filtrados.xlsx', index=False)
+
+
+
+
 def autenticar(username, password):
     ldap_server = 'COLOQUE O IP DO LDAP'                         #settings.LDAP_IP_SERVER
     ldap_domain = 'COLOQUE O DOMINIO'                              #settings.LDAP_DOMAIN_NAME
